603e9982413449ecc025f118a47f95834ffa9ef9
Eva Van Thienen <eva.vanthienen@verz.kbc.be>
2019-09-25 17:50:57 +0200

    trying to fix build issue

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const app = express();
+const app = new express();
 
 const { accounts, users, writeJSON } = require('./data.js');
 
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({extended: true }));
 
-app.get('/', (req,res) => res.render('index', {title: 'Account Summary', accounts}));
+app.get('/', (req,res) => res.render('index', {title: 'Account Summary', accounts: accounts}));
 
 app.get('/savings', (req,res) => res.render('account', {account: accounts.savings}));
 app.get('/checking', (req,res) => res.render('account', {account: accounts.checking}));
