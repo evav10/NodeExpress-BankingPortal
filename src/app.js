15782a381065b3e657a18e94138aa135ac959811
Eva Van Thienen <eva.vanthienen@verz.kbc.be>
2019-09-25 16:29:33 +0200

    trying to fix build issue

--- src/app.js
+++ src/app.js
@@ -1,37 +1,40 @@
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
-const app = express();
+const fs = require('fs');
+const path = require('path');
 
-const { accounts, users, writeJSON } = require('./data');
+const express = require('express');
+const app = new express();
 
-app.set('views',path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const { accounts, users, writeJSON } = require('./data.js');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true }));
+app.set('views',path.join(__dirname, '/views'));
+app.set('view engine','ejs');
 
-app.get('/', (req,res) => res.render('index', {title: 'Account Summary', accounts}));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/savings', (req,res) => res.render('account', {account: accounts.savings}));
-app.get('/checking', (req,res) => res.render('account', {account: accounts.checking}));
-app.get('/credit', (req,res) => res.render('account', {account: accounts.credit}));
-app.get('/profile', (req, res) => res.render('profile', {user: users[0]}));
-app.get('/transfer', (req, res) => res.render('transfer'));
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
+app.get('/transfer', (req, res) =>  res.render('transfer'));
+
 app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - 
-            req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + 
-            parseInt(req.body.amount, 10);
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
     writeJSON();
     res.render('transfer', {message: 'Transfer Completed'});
 });
-app.get('/payment', (req, res) => res.render('payment',{account: accounts.credit}));
+
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
+    accounts.credit.available += parseInt(req.body.amount);
     writeJSON();
-    res.render('payment', { message: 'Payment Successful', account: accounts.credit});
-})
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+});
 
-app.listen(3000, () => console.log("PS Project Running on port 3000!"));
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
+
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
