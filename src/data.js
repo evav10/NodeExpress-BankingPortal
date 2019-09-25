3c8f469b7498b7d6280faf7b2d972a69a69ea00b
Eva Van Thienen <eva.vanthienen@verz.kbc.be>
2019-09-25 18:02:56 +0200

    trying to fix build issue

--- src/data.js
+++ src/data.js
@@ -7,17 +7,13 @@ const accounts = JSON.parse(accountData);
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 const users = JSON.parse(userData);
 
-
-const accountsJSON = JSON.stringify(accounts, null, 4);
-fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
-
 const writeJSON = () => {
-    let accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
 }
 
-module.exports= {
-    accounts, 
-    users, 
-    writeJSON
+module.exports = {
+  accounts,
+  users,
+  writeJSON
 };
